1cc8c5ea0fa739de1fbfed22a40d2ad2e4a08cc6
devaspirant0510 <seungho020510@gmail.com>
2025-05-19 21:02:42 +0900

    feat : 로컬스토리지 json data 저장 함수 구현

---
+++ localstroageLib.js
@@ -0,0 +1,11 @@
+function saveData(jsonData,key="listItem"){
+    localStorage.setItem("listItem",JSON.stringify(jsonData));
+}
+function loadData(key="listItem"){
+    const data = localStorage.getItem("listItem");
+    if(!data){
+        throw Error("데이터를 불러오는데 실패했습니다. ");
+    }
+    return data;
+
+}

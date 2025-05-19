17359acac1f44f89f582966a2166fca8d4ae968c
devaspirant0510 <seungho020510@gmail.com>
2025-05-19 21:43:44 +0900

    feat: loaddata 함수 추가

--- localstroageLib.js
+++ localstroageLib.js
@@ -1,8 +1,8 @@
 function saveData(jsonData,key="listItem"){
-    localStorage.setItem("listItem",JSON.stringify(jsonData));
+    localStorage.setItem(key,JSON.stringify(jsonData));
 }
 function loadData(key="listItem"){
-    const data = localStorage.getItem("listItem");
+    const data = localStorage.getItem(key);
     
     if(!data){
         throw Error("데이터를 불러오는데 실패했습니다. ");

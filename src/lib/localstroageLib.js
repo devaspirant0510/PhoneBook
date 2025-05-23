cc2127be4c68f01a0ce1eb16bd4de5748a510947
devaspirant0510 <seungho020510@gmail.com>
2025-05-23 16:57:18 +0900

    feat: 전화 번호 조회 기능 구현

--- src/lib/localstroageLib.js
+++ src/lib/localstroageLib.js
@@ -1,11 +1,18 @@
-export  function saveData(jsonData,key="listItem"){
-    localStorage.setItem(key,JSON.stringify(jsonData));
+export function saveData(jsonData, key = "listItem") {
+    let result = jsonData.sort((a, b) => {
+        console.log(a, b)
+        return a.name.localeCompare(b.name)
+    });
+    console.log(result)
+    localStorage.setItem(key, JSON.stringify(result));
 }
-export function loadData(key="listItem"){
+
+export function loadData(key = "listItem") {
     const data = localStorage.getItem(key);
-    
-    if(!data){
-        throw Error("데이터를 불러오는데 실패했습니다. ");
+
+    if (!data) {
+        console.error("데이터를 불러오는데 실패했습니다. ");
+        return false;
     }
     return JSON.parse(data);
 
--- src/view/list/list.js
+++ src/view/list/list.js
@@ -1,24 +1,48 @@
-import "./index.css";
-import {loadData, saveData} from "./localstroageLib"
+import "./list.css";
+import {loadData, saveData} from "../../lib/localstroageLib"
 import jsonResult from "/phonebook.json";
+import PhoneItem from "../../components/PhoneItem";
+
 
 const list = document.querySelector("#list");
-saveData(jsonResult, "data");
-const data = loadData("data");
 
+let data = loadData("data");
+if(!data){
+    console.log("웹스토리지에 데이터가 없어  json 을 로드했다")
+    saveData(jsonResult, "data");
+    data = jsonResult;
+}
+function getInitialConsonant(char) {
+    const code = char.charCodeAt(0);
+
+    if (code < 0xac00 || code > 0xd7a3) {
+        return char// 한글이 아니면 prefix 안 붙이게!
+    }
+
+    const CHO = [
+        "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ",
+        "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ",
+        "ㅌ", "ㅍ", "ㅎ"
+    ];
+
+    const choIdx = Math.floor((code - 0xac00) / (21 * 28));
+    return CHO[choIdx];
+}
+let currentNamePrefix = ""
 data.forEach((element) => {
-    const item = document.createElement("div");
-    item.className = "phonebooke-item";
 
-    const profileImage = document.createElement("img");
-    profileImage.src = element.pic;
-    const porfileName = document.createElement("span");
-    porfileName.textContent = element.name;
-    const porfileHp = document.createElement("i");
-    porfileHp.className = "fa-solid fa-phone";
-    item.appendChild(profileImage)
-    item.appendChild(porfileName)
-    item.appendChild(porfileHp)
-    list.appendChild(item);
+    // 현재 섹션이 없는경우 첫번째 글자를 현재 섹션으로 설정
+    let prefix = getInitialConsonant(element.name[0])
+    if(currentNamePrefix !== prefix){
+        currentNamePrefix = prefix;
+        const div = document.createElement("div");
+
+        div.textContent = currentNamePrefix;
+        list.appendChild(div);
+
+    }
     console.log("element:" + element);
+    const phoneItem = PhoneItem(element.id, element.name, element.phone, element.email, element.pic);
+    list.appendChild(phoneItem);
+
 });
